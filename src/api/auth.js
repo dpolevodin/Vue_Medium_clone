03ae269983e901e48b05e103038651769840ae14
dpolevodin <dpolevodin@gmail.com>
2021-06-25 16:57:56 +0500

    add token

--- src/api/auth.js
+++ src/api/auth.js
@@ -7,7 +7,13 @@ const register = credentials => {
 const login = credentials => {
     return axios.post('/users/login', {user: credentials})
 }
+
+const getCurrentUser = () => {
+    return axios.get('/user')
+}
+
 export default {
     register,
-    login
+    login,
+    getCurrentUser
 }
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,13 @@
 import axios from 'axios'
+import {getItem} from '@/helpers/PersistanceStorage'
 
 axios.defaults.baseURL = 'https://conduit.productionready.io/api';
+
+axios.interceptors.request.use(config => {
+    const token = getItem('acessToken');
+    const authorizationToken = token ? 'Token ' + token : '';
+    config.headers.Authorization = authorizationToken;
+    return config;
+})
 
 export default axios;
