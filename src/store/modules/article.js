849d650fcc16745422c3757edab6c026ec8a379b
dpolevodin <dpolevodin@gmail.com>
2021-06-30 14:38:58 +0500

    add Article component

---
+++ src/store/modules/article.js
@@ -0,0 +1,55 @@
+import articleApi from "@/api/article"
+
+const state = {
+    data: null,
+    isLoading: false,
+    error: null
+}
+
+export const mutationTypes = {
+    getArticleStart: '[article] getArticleStart',
+    getArticleSuccess: '[article] getArticleSuccess',
+    getArticleFailure: '[article] getArticleFailure'
+}
+
+export const actionTypes = {
+    getArticle: '[article] getArticle',
+}
+
+
+const mutations = {
+    [mutationTypes.getArticleStart](state) {
+        state.isLoading = true,
+        state.data = null // очень важно, чтобы перетирать старый контент при выборе другой страницы
+    },
+    [mutationTypes.getArticleSuccess](state, payload) {
+        state.isLoading = false,
+        state.data = payload
+    },
+    [mutationTypes.getArticleFailure](state) {
+        state.isLoading = false
+    }
+}
+
+const actions = {
+    [actionTypes.getArticle](context, {slug}) {
+        return new Promise(resolve => {
+            context.commit(mutationTypes.getArticleStart, slug)
+            articleApi
+            .getArticle(slug)
+            .then(article  => {
+                context.commit(mutationTypes.getArticleSuccess, article)
+                resolve(article)
+            })
+            .catch(() => {
+                context.commit(mutationTypes.getArticleFailure)
+            })
+        })
+    }
+}
+
+export default {
+    state,
+    actions,
+    mutations
+}
